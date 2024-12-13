047cef81911d29a2921d5b02cc08449e13d6612c
Himal <himalmidun9@gmail.com>
2024-12-13 04:03:31 +0000

    adding like count

--- components/NavSearch.tsx
+++ components/NavSearch.tsx
@@ -1,16 +1,11 @@
 "use client"
 
-import React, { useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import Image from 'next/image'
 import { getProductTitles } from '@/lib/actions'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
-
-const navIcons = [
-  // {src: '/assets/icons/search.svg', alt:'search'},
-  { src: '/assets/icons/black-heart.svg', alt: 'heart' },
-  { src: '/assets/icons/user.svg', alt: 'user' },
-]
+import axios from 'axios'
 
 interface Props {
   productTitle: string,
@@ -26,7 +21,7 @@ const NavSearch = ({ titles }: NavSearchProps) => {
   const [isOptionVisible, setIsOptionVisible] = useState(false);
   // console.log('Titlesss: ', titles);
   const [filteredTitles, setFilteredTitles] = useState<Props[]>([]);
-  const inputRef =  useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLInputElement>(null);
   const dropdownRef = useRef<HTMLDivElement>(null);
   const handleBlur = (e: React.FocusEvent<HTMLInputElement>) => {
     setTimeout(() => {
@@ -46,18 +41,43 @@ const NavSearch = ({ titles }: NavSearchProps) => {
     else {
       setFilteredTitles([]);
     }
-
-
   }
+
+  const [likes,  setLikes] = useState(0);
+
+  useEffect(() => {
+    const fetchLikes = async () =>{
+        try {
+          const response = await axios.get('https://pricewise-like-count.123kingkim.workers.dev');
+          setLikes(response.data.likes);
+        } catch (error) {
+          console.log('Error fetching likes count: ', error);
+        }
+    }
+
+    fetchLikes();
+  }, [])
+
+  const handleLikeClick = async () => {
+    try {
+      
+        const response = await axios.post('https://pricewise-like-count.123kingkim.workers.dev')
+        setLikes(response.data.likes);
+      
+    } catch (error) {
+      console.log('Error handling like click: ', error);
+    }
+  }
+
   return (
     <div className='flex items-center gap-5'>
-      <div className='ml-10 relative'>
+      <div className='ml-10 relative' >
         <input ref={inputRef} type="text" className=' z-10 relative flex-1 w-9 h-9 text-sm p-2 pl-8 font-normal border border-gray-500 rounded-full
             focus:w-full focus:min-w-12 focus:pl-10 transition-all duration-100 ease-linear' placeholder='searching...'
           onChange={(e) => handleChange(e.target.value)}
           onFocus={() => setIsOptionVisible(true)}
-          onBlur={handleBlur} 
-          />
+          onBlur={handleBlur}
+        />
         <div ref={dropdownRef} className={`z-10 absolute w-56 max-h-21 lg:ml-3 sm:ml-0 overflow-y-scroll rounded-md bg-gray-100 pointer-events-auto ${isOptionVisible ? 'block' : 'hidden'}`} >
           {filteredTitles.length > 0 && (
             filteredTitles.map((filtered) => (
@@ -70,16 +90,6 @@ const NavSearch = ({ titles }: NavSearchProps) => {
               </Link>
             ))
           )}
-          {/* <div className='cursor-pointer py-3 px-3 hover:bg-slate-300'>
-            <p className='text-sm font-medium text-gray-600'>Title 1</p>
-          </div>
-          <div className='cursor-pointer py-3 px-3 hover:bg-slate-300'>
-            <p className='text-sm font-medium text-gray-600'>Title 2</p>
-          </div>
-          <div className='cursor-pointer py-3 px-3 hover:bg-slate-300'>
-            <p className='text-sm font-medium text-gray-600'>Title 2</p>
-          </div>
-        </div> */}
 
         </div>
         <div className='absolute z-20 inset-y-0 my-1.5 px-2 pointer-events-none'>
@@ -93,16 +103,26 @@ const NavSearch = ({ titles }: NavSearchProps) => {
       </div>
 
 
-      {navIcons.map((icon) => (
-        <Image
-          key={icon.alt}
-          src={icon.src}
-          alt={icon.alt}
-          width={28}
-          height={28}
-          className='object-contain'
-        />
-      ))}
+      <div className='flex flex-shrink-0'>
+        <button onClick={handleLikeClick}>
+          <Image
+            src='/assets/icons/black-heart.svg'
+            alt='heart'
+            width={28}
+            height={28}
+            className='object-contain'
+          />
+        </button>
+        <span className='text-sm text-black font-normal'>{likes}</span>
+      </div>
+
+      <Image
+        src='/assets/icons/user.svg'
+        alt='user'
+        width={28}
+        height={28}
+        className='object-contain'
+      />
 
     </div>
   )
